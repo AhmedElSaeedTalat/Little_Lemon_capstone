2d360cb9b7f1534c138032b9b088ba5429bf2551
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-11-30 18:13:19 +0200

    form booking

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Menu
+from .models import Bookings
 
 class BookingForm(forms.ModelForm):
     """ form for bookings """
     class Meta:
-        model = Menu
+        model = Bookings
         fields = '__all__'
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'please enter your name', 'class': 'form-control'}),
+            'slots': forms.NumberInput(attrs={'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'class': 'form-control date'})
+        }
+        
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from restaurant.forms import BookingForm
 
 # Create your views here.
 def home(request):
@@ -14,4 +15,13 @@ class BookView(View):
     """ class for booking tempates """
     def get(self, request):
         """ display booking page """
-        return render(request, 'book.html')
+        form = BookingForm()
+        context = {'form': form}
+        return render(request, 'book.html', context)
+    def post(self, request):
+        """ post booking data"""
+        form = BookingForm(request.POSt)
+        if form.is_valid():
+            form.save()
+        return 'success'
+        
